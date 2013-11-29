ae3a5930d2d194898daf9921384dcf95c59b8cc1
steelsojka <steelsojka@gmail.com>
2013-11-29 11:03:15 -0600

    fix(task): formatting

--- tasks/pdiffy.js
+++ tasks/pdiffy.js
@@ -10,22 +10,22 @@
 
 module.exports = function (grunt) {
 
-	var pdiffy = require("pdiffy");
+  var pdiffy = require("pdiffy");
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
   grunt.registerMultiTask('pdiffy', 'Perceptual difference tool for grunt', function () {
 
-		var done = this.async();
+    var done = this.async();
 
-		var onRunComplete = function(session) {
-			grunt.event.emit("pdiffy", this.target, session, done);
+    var onRunComplete = function(session) {
+      grunt.event.emit("pdiffy", this.target, session, done);
 
       // If running a schedule or interval don't call done
       !this.data.schedule && !this.data.interval && done();
-		};
+    };
 
-		pdiffy.run(this.data, onRunComplete.bind(this));	
+    pdiffy.run(this.data, onRunComplete.bind(this));  
   });
 };
